98b371f8ca91ce39dd4b297ff94f7a095fb3e79d
Qingqing Liu <qingqing.liu@salesforce.com>
2012-09-25 11:21:23 -0700

    Initial Commit

---
+++ NetworkSDK/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NetworkSDK
+//
+//  Created by Qingqing Liu on 9/24/12.
+//  Copyright (c) 2012 salesforce.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NetworkSDK/SFNetworkEngine.m
@@ -0,0 +1,13 @@
+//
+//  SFNetworkEngine.m
+//  NetworkSDK
+//
+//  Created by Qingqing Liu on 9/24/12.
+//  Copyright (c) 2012 salesforce.com. All rights reserved.
+//
+
+#import "SFNetworkEngine.h"
+
+@implementation SFNetworkEngine
+
+@end
