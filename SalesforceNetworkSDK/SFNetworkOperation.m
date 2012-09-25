f2e58f92d3f789c5dd6965d4d5f9e839abe395a7
Qingqing Liu <qingqing.liu@salesforce.com>
2012-09-25 16:38:45 -0700

    updated to become a static library project

---
+++ SalesforceNetworkSDK/SFNetworkOperation.m
@@ -0,0 +1,13 @@
+//
+//  SFNetworkOperation.m
+//  NetworkSDK
+//
+//  Created by Qingqing Liu on 9/25/12.
+//  Copyright (c) 2012 salesforce.com. All rights reserved.
+//
+
+#import "SFNetworkOperation.h"
+
+@implementation SFNetworkOperation
+
+@end
---
+++ SalesforceNetworkSDK/SFNetworkUtils.m
@@ -0,0 +1,14 @@
+//
+//  SFNetworkUtils.m
+//  NetworkSDK
+//
+//  Created by Qingqing Liu on 9/25/12.
+//  Copyright (c) 2012 salesforce.com. All rights reserved.
+//
+
+#import "SFNetworkUtils.h"
+
+@implementation SFNetworkUtils
+
+
+@end
